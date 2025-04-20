234481c01ae59d251a55aaff6fe9f359d487eb57
Jens Ouwens <99173882+The-Nibblers@users.noreply.github.com>
2025-04-20 10:50:44 +0200

    Refactored: begin spawning

--- Assets/script/spawnbeginarea.cs
+++ Assets/script/spawnbeginarea.cs
@@ -4,38 +4,31 @@ using UnityEngine;
 
 public class spawnbeginarea : MonoBehaviour
 {
-    public GameObject objectToSpawn;
-    public float radius = 10;
-    public Vector3 minPosition;
-    public Vector3 maxPosition;
-    public GameObject Thing;
-
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private float radius = 10;
+    [SerializeField] private Vector3 minPosition;
+    [SerializeField] private Vector3 maxPosition;
+    
+    private void Start()
     {
-
         StartCoroutine(time());
-
-        IEnumerator time()
-        {
-            Vector3 randomPosition = new Vector3(
-        Random.Range(minPosition.x, maxPosition.x),
-        Random.Range(minPosition.y, maxPosition.y),
-        Random.Range(minPosition.z, maxPosition.z)
-        );
-            int i = Random.Range(5, 10);
-
-            yield return new WaitForSeconds(i);
-            Thing = Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
-            Thing.tag = "Detect";
-            StartCoroutine(time());
-        }
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private IEnumerator time()
     {
+        Vector3 randomPosition = new Vector3(
+            Random.Range(minPosition.x, maxPosition.x),
+            Random.Range(minPosition.y, maxPosition.y),
+            Random.Range(minPosition.z, maxPosition.z)
+        );
         
+        int i = Random.Range(5, 10);
+
+        yield return new WaitForSeconds(i);
+
+        GameObject newHat = Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
+        newHat.tag = "Detect";
+        StartCoroutine(time());
     }
 
 }
